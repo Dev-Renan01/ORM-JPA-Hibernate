8b90add4d2177aae6a179519a9adfc0615f6da40
Thiago Renan <thiagorenan1108@gmail.com>
2025-08-20 00:48:41 -0300

    Criando a classe HibernateUtil e tastando

---
+++ java-maven-hibernate/src/test/java/java_maven_hibernate/java_maven_hibernate/TesteHibernate.java
@@ -0,0 +1,11 @@
+package java_maven_hibernate.java_maven_hibernate;
+
+import org.junit.Test;
+
+public class TesteHibernate{
+
+	@Test
+	public void testeHibernateUtil() {
+	HibernateUtil.getEntityManager();
+	}
+}
